512d0f31ed37a06b961d5bdcfcd4a0a527a4d5b5
orxan666 <orxanallahyarov03@gmail.com>
2022-11-15 21:23:38 +0400

    header bitti

---
+++ src/pages/Home.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import Helmet from '../components/Helmet/Helmet'
+
+const Home = () => {
+  return (
+    <Helmet title="home">
+    <div>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+      <h1>Ana sehife</h1>
+    </div>
+
+    </Helmet>
+  )
+}
+
+export default Home
